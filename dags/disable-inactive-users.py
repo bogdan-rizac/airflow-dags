d54a9eec261cf44cc9ed48526965e3f4f1de3825
Bogdan Rizac <bogdan-rizac@users.noreply.github.com>
2024-02-25 13:12:22 +0100

    update dag

--- dags/disable-inactive-users.py
+++ dags/disable-inactive-users.py
@@ -7,17 +7,18 @@ default_args = {
     'owner': 'airflow',
     'depends_on_past': False,
     'email': ['airflow@example.com'],
-    'email_on_failure': True,
+    'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
-    'start_date': datetime(2024, 2, 26),  # Adjust as needed
+    'start_date': datetime(2024, 2, 24),  # Adjust as needed
 }
 
 # Define the DAG
 with DAG(
     dag_id='deactivate_inactive_users',
     default_args=default_args,
+    catchup=False,
     schedule_interval='*/10 * * * *',  # Adjust as needed
 ) as dag:
 
